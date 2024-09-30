0c47d5542fd949e480d8dbf5f68d2d1bf1f300f5
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-09-30 22:13:03 +0400

    feat: add condition whether to render planet from object or from Component

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,13 @@ import OrbitLine from "./components/view/OrbitLine";
 import Controls from "./components/view/Controls";
 import PlanetInfo from "./components/view/PlanetInfo";
 import AsteroidBelt from "./components/planets/AsteroidBelt";
+import Earth from "./components/planets/Earth";
 
 // TODO: Add texture maps to planets
 const planets = [
 	{ name: "Mercury", radius: 0.383, semiMajorAxis: 5.7, eccentricity: 0.206, rotationSpeed: 0.01, orbitSpeed: 0.04, textureMap: "/textures/mercury.jpg", description: "Mercury is the smallest planet in the Solar System and the closest to the Sun." },
 	{ name: "Venus", radius: 0.949, semiMajorAxis: 10.8, eccentricity: 0.007, rotationSpeed: 0.004, orbitSpeed: 0.015, textureMap: "/textures/venus.jpg", description: "Venus is the second planet from the Sun and is Earth's closest planetary neighbor." },
-	{ name: "Earth", radius: 1, semiMajorAxis: 15, eccentricity: 0.017, rotationSpeed: 0.01, orbitSpeed: 0.01, textureMap: "/textures/earth_daymap.jpg", description: "Earth is the third planet from the Sun and the only astronomical object known to harbor life." },
+	{ name: "Earth", component: Earth, radius: 1, semiMajorAxis: 15, eccentricity: 0.017, rotationSpeed: 0.01, orbitSpeed: 0.01, textureMap: "/textures/earth_daymap.jpg", description: "Earth is the third planet from the Sun and the only astronomical object known to harbor life." },
 	{ name: "Mars", radius: 0.532, semiMajorAxis: 22.8, eccentricity: 0.093, rotationSpeed: 0.01, orbitSpeed: 0.008, textureMap: "/textures/mars.jpg", description: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System." },
 	{ name: "Jupiter", radius: 11.21, semiMajorAxis: 77.8, eccentricity: 0.048, rotationSpeed: 0.02, orbitSpeed: 0.002, textureMap: "/textures/jupiter.jpg", description: "Jupiter is the fifth planet from the Sun and the largest in the Solar System." },
 	{ name: "Saturn", radius: 9.45, semiMajorAxis: 143, eccentricity: 0.054, rotationSpeed: 0.018, orbitSpeed: 0.0009, textureMap: "/textures/saturn.jpg", description: "Saturn is the sixth planet from the Sun and the second-largest in the Solar System." },
@@ -51,22 +52,35 @@ function App() {
 				<PerspectiveCamera ref={cameraRef} makeDefault position={[0, 30, 50]} fov={75} />
 				<OrbitControls ref={controlsRef} />
 				<ambientLight intensity={0.1} />
-				<Stars radius={300} depth={60} count={20000} factor={7} saturation={0} fade />
+				<Stars radius={500} depth={60} count={20000} factor={7} saturation={0} fade />
 
 					<Sun />
 					<AsteroidBelt />
-					{planets.map((planet) => (
-						<Planet 
-							key={planet.name} 
-							{...planet} 
-							orbitRadius={planet.semiMajorAxis} 
-							isDynamic={isDynamic} 
-							speed={speed} 
-							onFocus={handlePlanetFocus} 
-							isFocused={focusedPlanet?.name === planet.name}
-							cameraRef={cameraRef}
-						/>
-					))}
+					{planets.map((planet) => 
+						planet.component ? (
+							<planet.component
+								key={planet.name}
+								{...planet}
+								orbitRadius={planet.semiMajorAxis}
+								isDynamic={isDynamic}
+								speed={speed}
+								onFocus={handlePlanetFocus}
+								isFocused={focusedPlanet?.name === planet.name}
+								cameraRef={cameraRef}
+							/>
+						) : (
+							<Planet 
+								key={planet.name} 
+								{...planet} 
+								orbitRadius={planet.semiMajorAxis} 
+								isDynamic={isDynamic} 
+								speed={speed} 
+								onFocus={handlePlanetFocus} 
+								isFocused={focusedPlanet?.name === planet.name}
+								cameraRef={cameraRef}
+							/>
+						)
+					)}
 					{planets.map((planet) => (
 						<OrbitLine key={planet.name} semiMajorAxis={planet.semiMajorAxis} eccentricity={planet.eccentricity} />
 					))}
