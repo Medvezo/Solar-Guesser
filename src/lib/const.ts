0f1c70a63bf980fb32d9f084b1266204c0c8262a
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-10-04 02:26:34 +0400

    media: add icons to /public and update src to const.ts primaryLayers

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -2,10 +2,10 @@ export const SUN_MASS = 1.989e30;
 export const G = 6.67430e-11;
 
 export const primaryLayers = [
-    { name: "Planets", icon: "○" },
-    { name: "Asteroids", icon: "△" },
-    { name: "Comets", icon: "□" },
-    { name: "Spacecrafts", icon: "⬟" },
+    { name: "Planets", icon: "○", src: "/icons/circleUI.png" },
+    { name: "Asteroids", icon: "△", src: "/icons/triangleUI.png" },
+    { name: "Comets", icon: "□", src: "/icons/squareUI.png" },
+    { name: "Spacecrafts", icon: "⬟", src: "/icons/pentagonUI.png" },
 ];
 
 export const secondaryLayers = [
