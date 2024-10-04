60408876e8cb474954f4183b87aa036718844c87
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-10-05 00:19:20 +0400

    feat: style adv and add reverse to time in TimeControls

--- src/components/ui/TimeControls.tsx
+++ src/components/ui/TimeControls.tsx
@@ -25,9 +25,8 @@ const TimeControls: React.FC<TimeControlsProps> = ({ speed, setSpeed }) => {
 	}, [speed]);
 
 	const handleSpeedChange = (value: number) => {
-		const speedOption = timeSpeedOptions.find(
-			(option) => option.value === value
-		);
+		const middleIndex = Math.floor(timeSpeedOptions.length / 2);
+		const speedOption = timeSpeedOptions[value + middleIndex];
 		setSpeed(speedOption ? speedOption.value : 1);
 	};
 
@@ -45,43 +44,95 @@ const TimeControls: React.FC<TimeControlsProps> = ({ speed, setSpeed }) => {
 	};
 
 	return (
-		<div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex items-center gap-4 bg-black bg-opacity-50 p-4 rounded-full">
-			<div className="flex flex-col items-center">
-				<Button onClick={() => setShowCalendar(!showCalendar)}>
-					{format(currentDateTime, "d MMM, yyyy")}
-				</Button>
-				{showCalendar && (
-					<div className="absolute bottom-full mb-2">
-						<Calendar
-							value={new CalendarDate(
-								currentDateTime.getFullYear(),
-								currentDateTime.getMonth() + 1,
-								currentDateTime.getDate()
-							)}
-							onChange={handleDateChange}
-						/>
-					</div>
-				)}
-			</div>
+		<div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex flex-col gap-4  p-4 rounded-full">
+			<section className="flex items-center gap-4">
+				<div className="flex flex-col items-center">
+					<Button onClick={() => setShowCalendar(!showCalendar)}>
+						{format(currentDateTime, "d MMM, yyyy")}
+					</Button>
+					{showCalendar && (
+						<div className="absolute bottom-full mb-2">
+							<Calendar
+								value={
+									new CalendarDate(
+										currentDateTime.getFullYear(),
+										currentDateTime.getMonth() + 1,
+										currentDateTime.getDate()
+									)
+								}
+								onChange={handleDateChange}
+							/>
+						</div>
+					)}
+				</div>
+
+				<div className="text-white min-w-[100px] text-center">
+					{speed < 0 ? "-" : ""}
+					{timeSpeedOptions.find((option) => option.value === Math.abs(speed))?.label || "Real Rate"}
+				</div>
+				<div className="text-white">{format(currentDateTime, "p")}</div>
+			</section>
+
 			<Slider
 				step={1}
-				maxValue={timeSpeedOptions.length - 1}
-				minValue={0}
+				maxValue={Math.floor(timeSpeedOptions.length / 2)}
+				minValue={-Math.floor(timeSpeedOptions.length / 2)}
 				defaultValue={0}
 				className="max-w-md"
-				onChange={(value) =>
-					handleSpeedChange(timeSpeedOptions[value as number].value)
-				}
-				marks={[
-					{ value: 0, label: "Real Rate" },
-					{ value: timeSpeedOptions.length - 1, label: "1 year/s" },
-				]}
+				onChange={(value) => handleSpeedChange(value as number)}
+				renderThumb={(props) => (
+					<div
+						{...props}
+						className="group p-1 top-1/2 rounded-full cursor-grab data-[dragging=true]:cursor-grabbing"
+					>
+						<span className="transition-transform bg-black  shadow-small  rounded-full w-5 h-5 block group-data-[dragging=true]:scale-80">
+							<svg
+								width="24"
+								height="24"
+								viewBox="0 0 41 41"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M20.5 37.5833C29.9349 37.5833 37.5834 29.9348 37.5834 20.5C37.5834 11.0651 29.9349 3.41663 20.5 3.41663C11.0652 3.41663 3.41669 11.0651 3.41669 20.5C3.41669 29.9348 11.0652 37.5833 20.5 37.5833Z"
+									stroke="#3CADD5"
+									strokeWidth="1.5"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								/>
+								<path
+									d="M13.6667 5.125H15.375C12.0438 15.1017 12.0438 25.8983 15.375 35.875H13.6667"
+									stroke="#3CADD5"
+									strokeWidth="1.5"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								/>
+								<path
+									d="M25.625 5.125C28.9563 15.1017 28.9563 25.8983 25.625 35.875"
+									stroke="#3CADD5"
+									strokeWidth="1.5"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								/>
+								<path
+									d="M5.125 27.3333V25.625C15.1017 28.9563 25.8983 28.9563 35.875 25.625V27.3333"
+									stroke="#3CADD5"
+									strokeWidth="1.5"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								/>
+								<path
+									d="M5.125 15.375C15.1017 12.0438 25.8983 12.0438 35.875 15.375"
+									stroke="#3CADD5"
+									strokeWidth="1.5"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								/>
+							</svg>
+						</span>
+					</div>
+				)}
 			/>
-			<div className="text-white min-w-[100px] text-center">
-				{timeSpeedOptions.find((option) => option.value === speed)?.label ||
-					"Real Rate"}
-			</div>
-			<div className="text-white">{format(currentDateTime, "p")}</div>
 		</div>
 	);
 };
