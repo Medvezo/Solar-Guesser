2968a02f9223c04d59576f005dd0e4eacfd9c128
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-10-04 17:00:05 +0400

    feat: add Framer motion animation to PlanetInfo as well

--- src/components/ui/PlanetInfo.tsx
+++ src/components/ui/PlanetInfo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface PlanetInfoProps {
 	planet: {
@@ -18,42 +19,50 @@ interface PlanetInfoProps {
 
 const PlanetInfo: React.FC<PlanetInfoProps> = ({ planet, onClose }) => {
 	return (
-		<div className="absolute top-1/2 right-28 transform -translate-y-1/2 bg-black bg-opacity-75 text-white p-6 rounded-2xl max-w-[400px]">
-			<h2 className="text-4xl font-bold mb-4 my-1 text-white">{planet.name}</h2>
-
-			<p className="text-[#716B6B] uppercase mb-1">{planet.type}</p>
-			<p className="mb-6 ">{planet.description}</p>
-			
-      <div className="space-y-2">
-				<h3 className="text-xl text-[#3CADD5] mb-2">Info</h3>
-				<InfoItem label="Moons" value={planet.moons} />
-				<InfoItem label="Gravity" value={planet.gravity} />
-				<InfoItem label="Length of the day" value={planet.dayLength} />
-				<InfoItem label="Radius" value={planet.radiusInKm} />
-				<InfoItem label="Orbital period" value={planet.orbitalPeriod} />
-				<InfoItem label="Distance from Sun" value={planet.distanceFromSun} />
-				<InfoItem label="Coordinates" value={planet.coordinates} />
-			</div>
-			<button
-				onClick={onClose}
-				className="absolute top-4 right-4 text-gray-400 hover:text-white"
+		<AnimatePresence>
+			<motion.div
+				initial={{ opacity: 0, x: 100, y: "-50%" }}
+				animate={{ opacity: 1, x: 0, y: "-50%" }}
+				exit={{ opacity: 0, x: 100, y: "-50%" }}
+				transition={{ duration: 0.3 }}
+				className="absolute top-1/2 right-28 bg-black bg-opacity-75 text-white p-6 rounded-2xl max-w-[400px]"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
+				<h2 className="text-4xl font-bold mb-4 my-1 text-white">{planet.name}</h2>
+
+				<p className="text-[#716B6B] uppercase mb-1">{planet.type}</p>
+				<p className="mb-6 ">{planet.description}</p>
+				
+				<div className="space-y-2">
+					<h3 className="text-xl text-[#3CADD5] mb-2">Info</h3>
+					<InfoItem label="Moons" value={planet.moons} />
+					<InfoItem label="Gravity" value={planet.gravity} />
+					<InfoItem label="Length of the day" value={planet.dayLength} />
+					<InfoItem label="Radius" value={planet.radiusInKm} />
+					<InfoItem label="Orbital period" value={planet.orbitalPeriod} />
+					<InfoItem label="Distance from Sun" value={planet.distanceFromSun} />
+					<InfoItem label="Coordinates" value={planet.coordinates} />
+				</div>
+				<button
+					onClick={onClose}
+					className="absolute top-4 right-4 text-gray-400 hover:text-white"
 				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						strokeWidth={2}
-						d="M6 18L18 6M6 6l12 12"
-					/>
-				</svg>
-			</button>
-		</div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M6 18L18 6M6 6l12 12"
+						/>
+					</svg>
+				</button>
+			</motion.div>
+		</AnimatePresence>
 	);
 };
 
