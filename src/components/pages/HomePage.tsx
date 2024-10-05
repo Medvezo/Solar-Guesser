6bdcf32c050e3f4535d192181acbab76599364b7
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-10-06 03:25:13 +0400

    style: homePage basic buttons styling

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
-      <div className="space-y-4">
-        <Link to="/explore" className="block px-6 py-3 bg-blue-600 text-white rounded-lg text-center hover:bg-blue-700 transition-colors">
-          Single Player
-        </Link>
-        <Link to="/multiplayer" className="block px-6 py-3 bg-green-600 text-white rounded-lg text-center hover:bg-green-700 transition-colors">
-          Multiplayer
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-start  justify-center h-screen bg-black text-white bg-[url('/images/HomeBG.png')] bg-cover bg-center mt-10 w-full">
+			<div className="mb-60 ">
+				<Link
+					to="/explore"
+					className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold text-center transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
+				>
+					Singleplayer
+				</Link>
+				<Link
+					to="/multiplayer"
+					className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold text-center transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
+				>
+					Multiplayer
+				</Link>
+				<Link
+					to="/explore"
+					className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold text-center transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
+				>
+					Solar Map
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
