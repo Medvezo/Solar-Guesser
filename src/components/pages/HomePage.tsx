025939370a0bad9c8e0ac46b6023ecf00661c9ed
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-10-06 07:39:22 +0400

    fix: styling issue minor

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 const HomePage: React.FC = () => {
 	return (
 		<>
-			<div className="flex flex-col items-start  justify-center h-screen bg-black text-white bg-[url('/images/HomeBG.png')] bg-cover bg-center mt-10 w-full">
+			<div className="flex flex-col items-start  justify-center h-screen bg-black text-white bg-[url('/images/HomeBG.png')] bg-cover bg-center mt-10 w-full ">
 				<div className="mb-60 ">
 					<Link
 						to="/singleplayer"
-						className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold text-center transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
+						className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold ml-12 transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
 					>
 						Singleplayer
 					</Link>
 					<Link
 						to="/multiplayer"
-						className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold text-center transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
+						className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold ml-12 transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
 					>
 						Multiplayer
 					</Link>
 					<Link
 						to="/explore"
-						className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold text-center transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
+						className="block audiowide w-80 py-4 bg-transparent text-white text-2xl font-bold ml-12 transition-colors uppercase tracking-widest hover:text-shadow-[0px_0px_4px_#FFFFFF]"
 					>
 						Solar Map
 					</Link>
